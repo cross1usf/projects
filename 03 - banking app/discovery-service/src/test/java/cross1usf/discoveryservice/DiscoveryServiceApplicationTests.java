07d52c6e4808587596d1fb61c1e4f3a5e57573fc
cross1usf <jakeedcross@gmail.com>
2022-06-06 20:46:14 -0400

    feat - new project

---
+++ 03 - banking app/discovery-service/src/test/java/cross1usf/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cross1usf.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
